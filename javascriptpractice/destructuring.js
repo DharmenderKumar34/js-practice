b75e8f194fc4a02226cd43f233936fbe67343c28
Dharmender_kumar <dharmenderkumar3465@gmail.com>
2025-06-18 10:40:28 +0530

    Add new content

---
+++ javascriptpractice/destructuring.js
@@ -0,0 +1,31 @@
+////array
+let arr=['vinod','thapa',26]
+// let fname=arr[0];
+// let lname=arr[1];
+// let age=arr[2];
+ 
+// console.log(fname);
+// console.log(lname);
+// console.log(age);
+
+// using this
+let[fname,lname,age]=arr;
+
+
+console.log(fname);
+console.log(lname);
+console.log(age);
+
+////object
+const obj={
+    Fname:"vinod",
+    Lname:"thapa",
+    Age:26
+}
+// let Age=obj.Age;
+// console.log(Age);
+
+let{Fname,Lname,Age}=obj;
+console.log(Fname);
+console.log(Lname);
+console.log(Age);
---
+++ javascriptpractice/spreadOperator.js
@@ -0,0 +1,7 @@
+const color=['red','green','blue','white']
+const colors=['red','green','blue','white','pink','black'] //this is not correct way(hard)
+const mycolor=[...color,'pink','yellow','black']
+
+console.log(color);
+console.log(colors);
+console.log(mycolor);
